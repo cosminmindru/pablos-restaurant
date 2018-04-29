f366f8ebcf28c25dbfa0b1201de5302f5b47944b
theSparrky <cosminmindrut@gmail.com>
2018-04-29 21:39:00 +0100

    added scrollreveal animations

--- src/js/script.js
+++ src/js/script.js
@@ -33,6 +33,56 @@ window.addEventListener('load', () => {
 	preloader.classList.add('animated');
 	setTimeout(() => {
 		preloader.classList.remove('shown');
-		theBody.classList.remove('no-scroll');
-	}, 300);
+	}, 700);
+
+	// SCROLLREVEAL CONFIG
+	window.sr = ScrollReveal();
+
+	function grabElements(elem){
+		return document.getElementsByClassName(elem);
+	}
+	
+	let fadeInTop = grabElements('fadeInTop'),
+		fadeInBottom = grabElements('fadeInBottom'),
+		fadeInRight = grabElements('fadeInRight'),
+		fadeInLeft = grabElements('fadeInLeft'),
+		fadeInTopSeq = grabElements('fadeInTopSeq');
+	
+	sr.reveal(fadeInTop, {
+		duration: 1500,
+		origin: 'top',
+		distance: '50vh',
+		viewFactor: 0.2
+	});
+	
+	sr.reveal(fadeInBottom, {
+		duration: 1500,
+		origin: 'bottom',
+		distance: '50vh',
+		viewFactor: 0.2
+	});
+	
+	sr.reveal(fadeInRight, {
+		duration: 1500,
+		origin: 'right',
+		distance: '50vw',
+		viewFactor: 0.2,
+		opacity: 0
+	});
+	
+	sr.reveal(fadeInLeft, {
+		duration: 1500,
+		origin: 'left',
+		distance: '50vw',
+		viewFactor: 0.2,
+		opacity: 0
+	});
+	
+	sr.reveal(fadeInTopSeq, {
+		duration: 1500,
+		origin: 'top',
+		distance: '100px',
+		viewFactor: 0.2,
+		opacity: 0
+	}, 100);
 });
